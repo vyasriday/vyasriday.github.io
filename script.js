0a0b33cb8a817c7b989371e54258a78220585351
Hridayesh Sharma <vyasriday7@gmail.com>
2019-03-26 00:10:07 +0530

    added responsiveness to hero section

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
 const header = document.getElementsByTagName('header')[0];
 const navigation = document.getElementById('navigation');
 const anchorInHeader = document.getElementsByClassName('header-links');
-
+const headerImage = document.getElementById('hero-icon');
 
 function changeLinkTextColorinHeader(links, color, fontWeight) {
   
   for(var i = 0; i< links.length; i++) {
     links[i].style.color = color;
     links[i].style.fontWeight = fontWeight;
-  }
-  
+  } 
 
+}
+
+function chnageHeaderIconSize(height, width, top, padding) {
+  headerImage.style.height = `${height}px`;
+  headerImage.style.width = `${width}px`;
+  headerImage.style.top = `${top}px`;
+  headerImage.style.padding = `${padding}px`;  
 }
 
 function fixNav() {
 
   if (window.scrollY >= header.scrollHeight) {
     navigation.classList.add('fixed-nav');
-    changeLinkTextColorinHeader(anchorInHeader, 'black', 600);
+    changeLinkTextColorinHeader(anchorInHeader, '#fff', 600);
+    chnageHeaderIconSize(80, 80, 0, 5);
+    
   } else {
     navigation.classList.remove('fixed-nav');
     changeLinkTextColorinHeader(anchorInHeader, 'white', 'unset');
+    chnageHeaderIconSize(100, 100, 10, 0);
   }
 
 }
