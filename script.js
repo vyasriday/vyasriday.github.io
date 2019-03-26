50922448391565fddbf4e75636ec12324c6c4f35
Hridayesh Sharma <vyasriday7@gmail.com>
2019-03-26 20:32:26 +0530

    added the version 1 for the website

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const header = document.getElementsByTagName('header')[0];
 const navigation = document.getElementById('navigation');
 const anchorInHeader = document.getElementsByClassName('header-links');
 const headerImage = document.getElementById('hero-icon');
+const projectsItems = document.getElementsByClassName('work-item');
 
 function changeLinkTextColorinHeader(links, color, fontWeight) {
   
@@ -21,7 +22,7 @@ function chnageHeaderIconSize(height, width, top, padding) {
 
 function fixNav() {
 
-  if (window.scrollY >= header.scrollHeight) {
+  if (window.scrollY >= header.scrollHeight - 150) {
     navigation.classList.add('fixed-nav');
     changeLinkTextColorinHeader(anchorInHeader, '#fff', 600);
     chnageHeaderIconSize(80, 80, 0, 5);
@@ -33,4 +34,10 @@ function fixNav() {
   }
 
 }
+
+function moveTop() {
+  header.scrollIntoView({behavior: "smooth"});
+}
 window.addEventListener('scroll', fixNav)
+
+headerImage.addEventListener('click', moveTop);
