5be16057ef09b305c0dc803df2c526ebaaafa2ee
Hridayesh Sharma <dev.hridayesh@gmail.com>
2020-05-09 17:00:14 +0530

    removed service worker

--- scripts/script.js
+++ scripts/script.js
@@ -4,51 +4,45 @@ const anchorInHeader = document.getElementsByClassName('header-links');
 const headerImage = document.getElementById('hero-icon');
 
 function changeLinkTextColorinHeader(color) {
-  
-  for(var i = 0; i< anchorInHeader.length; i++) {
+  for (var i = 0; i < anchorInHeader.length; i++) {
     anchorInHeader[i].style.color = color;
-  } 
-
+  }
 }
 
 function changeHeaderIconSize(height, width, top, padding) {
   headerImage.style.height = `${height}px`;
   headerImage.style.width = `${width}px`;
   headerImage.style.top = `${top}px`;
-  headerImage.style.padding = `${padding}px`;  
+  headerImage.style.padding = `${padding}px`;
 }
 
 function fixNav() {
-
   if (window.scrollY >= header.scrollHeight - 200) {
     navigation.classList.add('fixed-nav');
     changeLinkTextColorinHeader('#fff');
     changeHeaderIconSize(80, 80, 0, 5);
-    
   } else {
     navigation.classList.remove('fixed-nav');
     changeLinkTextColorinHeader('white');
     changeHeaderIconSize(100, 100, 10, 0);
   }
-
 }
 
 function moveTop() {
-  header.scrollIntoView({behavior: "smooth"});
+  header.scrollIntoView({ behavior: 'smooth' });
 }
 
-
-window.addEventListener('scroll', fixNav)
+window.addEventListener('scroll', fixNav);
 
 headerImage.addEventListener('click', moveTop);
 
 //  Register Service Worker
 
-if ('serviceWorker' in navigator) {
+// if ('serviceWorker' in navigator) {
 
-  navigator.serviceWorker
-    .register('/sw.js')
-    .then(reg => console.log("Service Worker Registered", reg))
-    .catch(err => console.log("Service Worker Failed to Register", err))
+//   navigator.serviceWorker
+//     .register('/sw.js')
+//     .then(reg => console.log("Service Worker Registered", reg))
+//     .catch(err => console.log("Service Worker Failed to Register", err))
 
-}
+// }
